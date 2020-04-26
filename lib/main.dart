19b94be94c6e8f16439c0823ab1dc41d3bf468a4
Timothy Liu <timsgithub@gmail.com>
2020-04-26 12:47:13 -0700

    Create base Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SteppingBee());
+
+class SteppingBee extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
